8b4b486d0618c280d5172f65d001f5f00b43ac06
Genesis Doge <doge666@generative.xyz>
2022-12-23 13:33:29 +0700

    feat/project - create project

--- internal/entity/projects.go
+++ internal/entity/projects.go
@@ -9,7 +9,30 @@ import (
 type Projects struct {
 	BaseEntity`bson:",inline"`
 	ContractAddress string `bson:"contractAddress"`
-	TokenID string `bson:"tokenID"`
+	TokenID string `bsonbson:"tokenID"`
+	MaxSupply int `bson:"maxSupply"`
+	LimitSupply int `bson:"limitSupply"`
+	MintPrice string `bson:"mintPrice"`
+	Name string `bson:"name"`
+	CreatorName string `bson:"creatorName"`
+	Description string `bson:"description"`
+	Thumbnail string `bson:"thumbnail"`
+	ThirdPartyScripts []string `bson:"thirdPartyScripts"`
+	Scripts []string `bson:"scripts"`
+	ReservationList []string `bson:"reservationList"`
+	MintFee int `bson:"mintFee"`
+	OpenMintUnixTimestamp int `bson:"openMintUnixTimestamp"`
+	TokenDescription string `bson:"tokenDescription"`
+	Styles string `bson:"styles"`
+	Royalty int `bson:"royalty"`
+	SocialWeb string `bson:"socialWeb"`
+	SocialTwitter string `bson:"socialTwitter"`
+	SocialDiscord string `bson:"socialDiscord"`
+	SocialMedium string `bson:"socialMedium"`
+	SocialInstagram string `bson:"socialInstagram"`
+	License string `bson:"license"`
+	MintTokenAddress string `bson:"mintTokenAddress"`
+	Hash string `bson:"hash"`
 	Tags []string `bson:"tags"`
 	Categories []string `bson:"categories"`
 }
--- internal/repository/projects.go
+++ internal/repository/projects.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"encoding/json"
+
 	"rederinghub.io/internal/entity"
 	"rederinghub.io/utils"
 	"rederinghub.io/utils/helpers"
@@ -10,16 +12,41 @@ import (
 
 func (r Repository) FindProject( projectID string) (*entity.Projects, error) {
 	resp := &entity.Projects{}
-
 	usr, err := r.FilterOne(entity.Projects{}.TableName(), bson.D{{utils.KEY_UUID, projectID}})
 	if err != nil {
 		return nil, err
 	}
-
+	
 	err = helpers.Transform(usr, resp)
 	if err != nil {
 		return nil, err
 	}
+	return resp, nil
+}
+
+func (r Repository) FindProjectBy( contractAddress string, tokenID string) (*entity.Projects, error) {
+	resp := &entity.Projects{}
+
+	p, err := r.Cache.GetData(helpers.ProjectDetailKey(contractAddress, tokenID))
+	if err != nil {
+		usr, err := r.FilterOne(entity.Projects{}.TableName(), bson.D{{"contractAddress", contractAddress}, {"tokenID", tokenID}})
+		if err != nil {
+			return nil, err
+		}
+	
+		err = helpers.Transform(usr, resp)
+		if err != nil {
+			return nil, err
+		}
+		return resp, nil
+	}
+
+	bytes := []byte(*p)
+	err = json.Unmarshal(bytes, resp)
+	if err != nil {
+		return nil, err
+	}
+
 	return resp, nil
 }
 
@@ -29,6 +56,20 @@ func (r Repository) CreateProject(data *entity.Projects) error {
 	if err != nil {
 		return err
 	}
+
+	_ = r.Cache.SetData(helpers.ProjectDetailKey(data.ContractAddress, data.TokenID), data)
+
+	return nil
+}
+
+func (r Repository) UpdateProject( data *entity.Projects) error {
+
+	err := r.InsertOne(data.TableName(), data)
+	if err != nil {
+		return err
+	}
+
+	_ = r.Cache.SetData(helpers.ProjectDetailKey(data.ContractAddress, data.TokenID), data)
 
 	return nil
 }
