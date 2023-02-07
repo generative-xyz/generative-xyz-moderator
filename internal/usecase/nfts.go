6b4e81c69082be1989756f39fa67fe76de45e6b1
Neraxis <neraxis@generative.xyz>
2023-02-07 17:10:18 +0700

    add api to get leaderboard from mông

--- internal/usecase/nfts.go
+++ internal/usecase/nfts.go
@@ -3,8 +3,6 @@ package usecase
 import (
 	"errors"
 	"math/big"
-	"sort"
-	"strings"
 	"time"
 
 	"github.com/opentracing/opentracing-go"
@@ -64,85 +62,19 @@ func (u Usecase) GetTokenHolders(rootSpan opentracing.Span, req structure.GetTok
 	defer u.Tracer.FinishSpan(span, log)
 
 	log.SetData("req", req)
-	covalentResp, err := u.CovalentNft.GetTokenHolder(nftStructure.CovalentGetTokenHolderRequest{
-		Chain:           req.Chain,
-		ContractAddress: req.ContractAddress,
-		Page:            req.Page,
-		Limit:           req.Limit,
+
+	bf := entity.BaseFilters{
+		Page: int64(req.Page),
+		Limit: int64(req.Limit),
+		SortBy: "current_rank",
+		Sort: entity.SORT_ASC,
+	}
+
+	resp, err := u.Repo.FilterTokenHolders(entity.FilterTokenHolders{
+		BaseFilters: bf,
 	})
-	if err != nil {
-		return nil, err
-	}
 
-	tokenHolders := make([]structure.TokenHolder, 0)
-
-	getProfile := func(profileChan chan structure.ProfileChan, address string) {
-		var user *entity.Users
-		var err error
-
-		defer func() {
-			profileChan <- structure.ProfileChan{
-				Data: user,
-				Err: err,
-			}
-		}()
-
-		user, err = u.GetUserProfileByWalletAddress(span, strings.ToLower(address))
-		if err != nil {
-			return
-		}
-	}
-
-	profileChans := make([]chan structure.ProfileChan, 0)
-
-	for _, item := range covalentResp.Data.Items {
-		tokenHolders = append(tokenHolders, structure.TokenHolder{
-			ContractDecimals:     item.ContractDecimals,
-			ContractName:         item.ContractName,
-			ContractTickerSymbol: item.ContractTickerSymbol,
-			ContractAddress:      item.ContractAddress,
-			SupportsErc:          item.SupportsErc,
-			LogoURL:              item.LogoURL,
-			Address:              item.Address,
-			Balance:              item.Balance,
-			TotalSupply:          item.TotalSupply,
-			BlockHeight:          item.BlockHeight,
-		})
-		profileChan := make(chan structure.ProfileChan, 1)
-		go getProfile(profileChan, item.Address)
-		profileChans = append(profileChans, profileChan)
-	}
-
-	for i := 0; i < len(tokenHolders); i++ {
-		usrFromChan := <-profileChans[i]
-		tokenHolders[i].Profile = usrFromChan.Data
-	}
-
-	sort.SliceStable(tokenHolders, func(i, j int) bool {
-		lhs := new(big.Int)
-		lhs, ok := lhs.SetString(tokenHolders[i].Balance, 10)
-		if !ok {
-			lhs = big.NewInt(0)
-		}
-		rhs := new(big.Int)
-		rhs, ok = rhs.SetString(tokenHolders[j].Balance, 10)
-		if !ok {
-			rhs = big.NewInt(0)
-		}
-		return lhs.Cmp(rhs) > 0
-	})
-	total, ok := covalentResp.Data.Pagination.TotalCount.(int)
-	if !ok {
-		total = len(tokenHolders)
-	}
-	resp := &entity.Pagination{
-		Page:   int64(req.Page),
-		PageSize:  int64(req.Limit),
-		Result: tokenHolders,
-		Total: int64(total),
-	}
-
-	return resp, nil
+	return resp, err
 }
 
 func (u Usecase) GetNftMintedTime(rootSpan opentracing.Span, req structure.GetNftMintedTimeReq) (*structure.NftMintedTime, error) {
