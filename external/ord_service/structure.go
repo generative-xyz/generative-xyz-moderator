7eded4bde85f814b6f6165c3a598dd22285ed6d4
tri luong <tri@autonomous.nyc>
2023-02-08 17:41:32 +0700

    Feat/btc - fix bug

---
+++ external/ord_service/structure.go
@@ -0,0 +1,12 @@
+package ord_service
+
+type ExecRequest struct {
+	Args []string `json:"args"`
+}
+
+type ExecRespose struct {
+	Message string `json:"message"`
+	Stdout string `json:"stdout"`
+	Stderr string `json:"stderr"`
+	ErrorCode string `json:"errorCode"`
+}
