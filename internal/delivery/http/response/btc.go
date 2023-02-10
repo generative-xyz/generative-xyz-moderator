48713b5061594401104e0a2c927f20b25733ec5e
Neraxis <neraxis@generative.xyz>
2023-02-10 16:41:25 +0700

    fix return wallet resp

--- internal/delivery/http/response/btc.go
+++ internal/delivery/http/response/btc.go
@@ -3,7 +3,7 @@ package response
 type BctWalletResp struct {
 	BaseResponse
 	UserAddress string `json:"user_address"` //user's wallet address from FE
-	Amount string `bson:"amount"`
+	Amount string `json:"amount"`
 	OrdAddress string `json:"ordAddress"` // address is generated from ORD service, which receive all amount
 	FileURI string `json:"fileURI"` // FileURI will be mount if OrdAddress get all amount
 	IsConfirm bool  `json:"isConfirm"` //default: false, if OrdAddress get all amount it will be set true
@@ -22,9 +22,9 @@ type BctReceiveWalletResp struct {
 type BtcWalletRespV2 struct {
 	BaseResponse
 	UserAddress string `json:"userAddress"` //user's wallet address from FE
-	Amount string `bson:"amount"`
-	MintFee string `bson:"mintFee"`
-	SentTokenFee string `bson:"sentTokenFee"`
+	Amount string `json:"amount"`
+	MintFee string `json:"mintFee"`
+	SentTokenFee string `json:"sentTokenFee"`
 	OrdAddress string `json:"ordAddress"` // address is generated from ORD service, which receive all amount
 	FileURI string `json:"fileURI"` // FileURI will be mount if OrdAddress get all amount
 	IsConfirm bool  `json:"isConfirm"` //default: false, if OrdAddress get all amount it will be set true
