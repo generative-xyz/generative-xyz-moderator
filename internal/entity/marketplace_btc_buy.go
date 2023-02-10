a1c3b4db8806f5acca1309eecf21a6d8e686bbf0
Genesis Doge <doge666@generative.xyz>
2023-02-10 21:44:47 +0700

    update data

--- internal/entity/marketplace_btc_buy.go
+++ internal/entity/marketplace_btc_buy.go
@@ -1,6 +1,8 @@
 package entity
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"rederinghub.io/utils"
 	"rederinghub.io/utils/helpers"
@@ -8,11 +10,12 @@ import (
 
 type MarketplaceBTCBuyOrder struct {
 	BaseEntity    `bson:",inline"`
-	OrdAddress    string `bson:"ord_address"`
-	ItemID        string `bson:"item_id"`
-	InscriptionID string `bson:"inscriptionID"` // tokenID in btc
-	SegwitAddress string `bson:"segwit_address"`
-	SegwitKey     string `bson:"segwit_key"`
+	OrdAddress    string    `bson:"ord_address"`
+	ItemID        string    `bson:"item_id"`
+	InscriptionID string    `bson:"inscriptionID"` // tokenID in btc
+	SegwitAddress string    `bson:"segwit_address"`
+	SegwitKey     string    `bson:"segwit_key"`
+	ExpiredAt     time.Time `bson:"expired_at"`
 }
 
 func (u MarketplaceBTCBuyOrder) TableName() string {
