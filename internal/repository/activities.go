a2cbd585bf03a57263457c5e25af93815e81ca18
Neraxis <neraxis@generative.xyz>
2023-02-16 18:07:21 +0700

    cronjob to update trending score

--- internal/repository/activities.go
+++ internal/repository/activities.go
@@ -1,7 +1,12 @@
 package repository
 
 import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"rederinghub.io/internal/entity"
+	"rederinghub.io/utils"
 )
 
 func (r Repository) InsertActitvy(data *entity.Activity) error {
@@ -10,4 +15,40 @@ func (r Repository) InsertActitvy(data *entity.Activity) error {
 		return err
 	}
 	return nil
+}
+
+// Get all BTC activity (Mint, Buy)
+func (r Repository) GetRecentBTCActivity() ([]entity.Activity, error) {
+	activities := []entity.Activity{}
+
+	f := bson.M{
+		"type": bson.M{"$in": []entity.ActivityType{entity.Mint, entity.Buy}},
+		"created_at": bson.M{"$gte":  time.Now().Add(-24*time.Hour).UTC()},
+	}
+
+	cursor, err := r.DB.Collection(utils.COLLECTION_ACTIVITIES).Find(context.TODO(), f)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = cursor.All(context.TODO(), &activities); err != nil {
+		return nil, err
+	}
+
+	return activities, nil
+}
+
+func (r Repository) CountViewActivity(projectID string) (*int64, error) {
+	f := bson.M{
+		"type": entity.View,
+		"created_at": bson.M{"$gte":  time.Now().Add(-24*time.Hour).UTC()},
+		"project_id": projectID,
+	}
+
+	count, err := r.DB.Collection(utils.COLLECTION_ACTIVITIES).CountDocuments(context.TODO(), f)
+	if err != nil {
+		return nil, err
+	}
+
+	return &count, nil
 }
