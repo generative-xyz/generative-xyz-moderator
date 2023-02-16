b012c23978af5bca6471eba99995d7e56bb8a467
Genesis Doge <doge666@generative.xyz>
2023-02-16 10:32:22 +0700

    Comment code to build

--- internal/usecase/auth.go
+++ internal/usecase/auth.go
@@ -132,14 +132,15 @@ func (u Usecase) VerifyMessage(rootSpan opentracing.Span, data structure.VerifyM
 		return nil, err
 	}
 
-	if data.AddressBTC !=  nil {
-		if *data.AddressBTC  != "" {
-			if user.WalletAddressBTC == ""  {
-				user.WalletAddressBTC = *data.AddressBTC
-				log.SetData("user.WalletAddressBTC.Updated", true)
-			}
-		}
-	}
+	//TODO - Tri temporay comment
+	// if data.AddressBTC !=  nil {
+	// 	if *data.AddressBTC  != "" {
+	// 		if user.WalletAddressBTC == ""  {
+	// 			user.WalletAddressBTC = *data.AddressBTC
+	// 			log.SetData("user.WalletAddressBTC.Updated", true)
+	// 		}
+	// 	}
+	// }
 
 	updated, err := u.Repo.UpdateUserByWalletAddress(user.WalletAddress, user)
 	if err != nil {
