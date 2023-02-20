87c156f0ab81f06fae2d1fcb63f7a45f72d98de3
genesis-0000 <thaibao@autonomous.nyc>
2023-02-20 13:01:33 +0700

    - btc wallet from eth wallet -> verify -> debug

--- utils/helpers/btc.go
+++ utils/helpers/btc.go
@@ -1,7 +1,6 @@
 package helpers
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"encoding/base64"
 	"github.com/btcsuite/btcd/btcec/v2"
@@ -9,7 +8,6 @@ import (
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
 	"golang.org/x/crypto/ripemd160"
 	"hash"
 )
@@ -36,7 +34,6 @@ func GetAddressFromPubKey(publicKey *btcec.PublicKey, compressed bool) (*btcutil
 
 // PubKeyFromSignature gets a publickey for a signature and tells you whether is was compressed
 func PubKeyFromSignature(sig, msg string, prefix string) (pubKey *btcec.PublicKey, wasCompressed bool, err error) {
-	// TODO hiennguyen
 	var decodedSig []byte
 	if decodedSig, err = base64.StdEncoding.DecodeString(sig); err != nil {
 		return nil, false, err
@@ -46,62 +43,15 @@ func PubKeyFromSignature(sig, msg string, prefix string) (pubKey *btcec.PublicKe
 	if err != nil {
 		return nil, false, err
 	}
-	aaa := []byte{
-		89,
-		217,
-		162,
-		121,
-		96,
-		82,
-		197,
-		186,
-		9,
-		206,
-		127,
-		206,
-		247,
-		56,
-		104,
-		197,
-		70,
-		22,
-		253,
-		55,
-		13,
-		145,
-		1,
-		144,
-		223,
-		46,
-		73,
-		71,
-		6,
-		111,
-		174,
-		40,
-	}
-	_ = temp
-	k, c, err := ecdsa.RecoverCompact(decodedSig, aaa[:])
+	k, c, err := ecdsa.RecoverCompact(decodedSig, temp[:])
 	return k, c, err
 }
 
-func MagicHash(msg, messagePrefix string) (*chainhash.Hash, error) {
-	// TODO hiennguyen
+func MagicHash(msg, messagePrefix string) (chainhash.Hash, error) {
 	if messagePrefix == "" {
-		messagePrefix = `\u0018Bitcoin Signed Message:\n`
+		messagePrefix = "\u0018Bitcoin Signed Message:\n"
 	}
 
-	// Validate the signature - this just shows that it was valid at all
-	// we will compare it with the key next
-	var buf bytes.Buffer
-	if err := wire.WriteVarString(&buf, 0, messagePrefix); err != nil {
-		return nil, err
-	}
-	if err := wire.WriteVarString(&buf, 0, msg); err != nil {
-		return nil, err
-	}
-
-	// Create the hash
-	expectedMessageHash := chainhash.HashH(buf.Bytes())
-	return &expectedMessageHash, nil
+	bytes := append([]byte(messagePrefix), []byte(msg)...)
+	return chainhash.DoubleHashH(bytes), nil
 }
