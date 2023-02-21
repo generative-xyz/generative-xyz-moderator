d5799b4fd041f66f02b62844b85d23254f47e066
Genesis Doge <doge666@generative.xyz>
2023-02-21 18:37:02 +0700

    update job config

---
+++ internal/delivery/crontab_mint_nft_btc/crontab.go
@@ -0,0 +1,73 @@
+package mint_nft_btc
+
+import (
+	"sync"
+	"time"
+
+	"rederinghub.io/internal/usecase"
+	"rederinghub.io/utils/global"
+	"rederinghub.io/utils/logger"
+	"rederinghub.io/utils/redis"
+)
+
+type CronMintNftBtcHandler struct {
+	Logger logger.Ilogger
+
+	Cache   redis.IRedisCache
+	Usecase usecase.Usecase
+}
+
+func NewCronMintNftBtcHandler(global *global.Global, uc usecase.Usecase) *CronMintNftBtcHandler {
+	return &CronMintNftBtcHandler{
+		Logger:  global.Logger,
+		Cache:   global.Cache,
+		Usecase: uc,
+	}
+}
+
+func (h CronMintNftBtcHandler) StartServer() {
+
+	var wg sync.WaitGroup
+
+	for {
+		wg.Add(5)
+
+		// job check balance:
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			h.Usecase.JobMint_CheckBalance()
+
+		}(&wg)
+
+		// job mint nft:
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			h.Usecase.JobMint_MintNftBtc()
+
+		}(&wg)
+
+		// job check tx:
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			h.Usecase.JobMint_CheckTxMintSend()
+
+		}(&wg)
+
+		// job send btc to ord address:
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			h.Usecase.JobMin_SendNftToUser()
+
+		}(&wg)
+
+		// job send nft to user:
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			h.Usecase.JobMin_SendNftToUser()
+
+		}(&wg)
+		h.Logger.Info("wait", "wait")
+		wg.Wait()
+		time.Sleep(5 * time.Minute)
+	}
+}
