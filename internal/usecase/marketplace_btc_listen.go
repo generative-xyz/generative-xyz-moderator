950608e3bbe36807a1990206a179fb6228e60c3a
Genesis Doge <doge666@generative.xyz>
2023-02-21 16:30:47 +0700

    Merge conflict

--- internal/usecase/marketplace_btc_listen.go
+++ internal/usecase/marketplace_btc_listen.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"math/big"
 	"strings"
+	"time"
 
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
 	"rederinghub.io/external/ord_service"
+	"rederinghub.io/internal/delivery/http/request"
 	"rederinghub.io/internal/entity"
 	"rederinghub.io/internal/usecase/structure"
 	"rederinghub.io/utils"
@@ -368,11 +370,13 @@ func (u Usecase) BtcSendBTCForBuyOrder() error {
 			destinations := make(map[string]int)
 
 			destinations[nftListing.SellerAddress] = amountWithChargee
-			if artistAddress != "" {
+			if artistAddress != "" && royaltyFee > 0 {
 				destinations[artistAddress] = royaltyFee
 			}
 
-			destinations[serviceFeeAddress] = serviceFee
+			if serviceFee > 0 {
+				destinations[serviceFeeAddress] = serviceFee
+			}
 
 			txFee, err := bs.EstimateFeeTransactionWithPreferenceFromSegwitAddressMultiAddress(item.SegwitKey, item.SegwitAddress, destinations, btc.PreferenceMedium)
 			if err != nil {
@@ -764,4 +768,45 @@ func (u Usecase) SendTokenMKPTest( walletName, receiveAddr, inscriptionID string
 	go u.trackHistory("test_send_nft", "SendTokenMKPTest", "", 0, "", "return now...")
 
 	return resp, err
+}
+
+// admin
+// check receive of the nft:
+func (u Usecase) AutoListing(reqs *request.ListNftIdsReq) interface{} {
+	var listIdSuccess []string
+
+	if reqs != nil {
+		for _, v := range reqs.InscriptionID {
+			//v.Inscription
+			listing := entity.MarketplaceBTCListing{
+				SellOrdAddress: reqs.SellOrdAddress,
+				SellerAddress:  reqs.SellerAddress,
+				HoldOrdAddress: "",
+				ServiceFee:     "0",
+				Price:          reqs.Price,
+				IsConfirm:      true,
+				IsSold:         false,
+				ExpiredAt:      time.Now().Add(time.Hour * 1),
+				Name:           "",
+				Description:    "",
+				InscriptionID:  v,
+			}
+			// get first:
+			nftList, _ := u.Repo.FindBtcNFTListingByNFTID(v)
+			if nftList != nil && nftList.IsConfirm && !nftList.IsSold {
+				u.Logger.Error("AutoListing.Repo.FindBtcNFTListingByNFTID", "", errors.New("item exist"))
+				continue
+			}
+
+			// check if listing is created or not
+			err := u.Repo.CreateMarketplaceListingBTC(&listing)
+			if err != nil {
+				u.Logger.Error("AutoListing.Repo.CreateMarketplaceBTCListing", "", err)
+				continue
+			}
+			listIdSuccess = append(listIdSuccess, v)
+		}
+	}
+
+	return listIdSuccess
 }
