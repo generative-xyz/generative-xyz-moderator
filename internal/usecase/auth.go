aaae2fcb170f6388596f0b7b4378575135aa7cd0
Genesis Doge <doge666@generative.xyz>
2023-02-24 12:22:15 +0700

    - profile by wallet btc taproot

--- internal/usecase/auth.go
+++ internal/usecase/auth.go
@@ -251,13 +251,37 @@ func (u Usecase) GetUserProfileByWalletAddress(userAddr string) (*entity.Users, 
 	return user, nil
 }
 
-func (u Usecase) UpdateUserProfile( userID string, data structure.UpdateProfile) (*entity.Users, error) {
-	
+func (u Usecase) GetUserProfileByBtcAddressTaproot(userAddr string) (*entity.Users, error) {
+
+	u.Logger.Info("input.userAddr", userAddr)
+	user, err := u.Repo.FindUserByBtcAddressTaproot(userAddr)
+	if err != nil {
+		u.Logger.Error(err)
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u Usecase) GetUserProfileByBtcAddress(userAddr string) (*entity.Users, error) {
+
+	u.Logger.Info("input.userAddr", userAddr)
+	user, err := u.Repo.FindUserByBtcAddress(userAddr)
+	if err != nil {
+		u.Logger.Error(err)
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u Usecase) UpdateUserProfile(userID string, data structure.UpdateProfile) (*entity.Users, error) {
+
 	isUpdateWalletAddress := false
 	oldBtcAdress := ""
 	user, err := u.Repo.FindUserByID(userID)
 	if err != nil {
-		u.Logger.ErrorAny("UpdateUserProfile",zap.String("action", "FindUserByID"), zap.String("userID", userID), zap.Any("data", data), zap.Error(err))
+		u.Logger.ErrorAny("UpdateUserProfile", zap.String("action", "FindUserByID"), zap.String("userID", userID), zap.Any("data", data), zap.Error(err))
 		return nil, err
 	}
 
@@ -269,7 +293,7 @@ func (u Usecase) UpdateUserProfile( userID string, data structure.UpdateProfile)
 		user.Avatar = *data.Avatar
 		uploaded, err := u.UploadUserAvatar(*user)
 		if err != nil {
-			u.Logger.ErrorAny("UpdateUserProfile",zap.String("action", "UploadUserAvatar"), zap.String("userID", userID), zap.Any("data", data), zap.Error(err))
+			u.Logger.ErrorAny("UpdateUserProfile", zap.String("action", "UploadUserAvatar"), zap.String("userID", userID), zap.Any("data", data), zap.Error(err))
 		} else {
 			user.Avatar = *uploaded
 		}
@@ -280,7 +304,7 @@ func (u Usecase) UpdateUserProfile( userID string, data structure.UpdateProfile)
 		user.Bio = *data.Bio
 	}
 
-	if data.WalletAddressBTC != nil && strings.ToLower(user.WalletAddressBTC) != strings.ToLower( *data.WalletAddressBTC) {
+	if data.WalletAddressBTC != nil && strings.ToLower(user.WalletAddressBTC) != strings.ToLower(*data.WalletAddressBTC) {
 		isUpdateWalletAddress = true
 		oldBtcAdress = user.WalletAddressBTC
 		user.WalletAddressBTC = *data.WalletAddressBTC
@@ -329,7 +353,7 @@ func (u Usecase) UpdateUserProfile( userID string, data structure.UpdateProfile)
 
 		u.Logger.LogAny("UpdateUserProfile", zap.Any("projects", projects))
 		if err != nil {
-			u.Logger.ErrorAny("UpdateUserProfile",zap.String("action", "GetAllProjects"), zap.String("userID", userID), zap.Any("data", data), zap.Error(err))
+			u.Logger.ErrorAny("UpdateUserProfile", zap.String("action", "GetAllProjects"), zap.String("userID", userID), zap.Any("data", data), zap.Error(err))
 			return
 		}
 
@@ -341,18 +365,17 @@ func (u Usecase) UpdateUserProfile( userID string, data structure.UpdateProfile)
 
 			_, err := u.Repo.UpdateProject(p.UUID, &p)
 			if err != nil {
-				u.Logger.ErrorAny("UpdateUserProfile",zap.String("action", "GetAllProjects"), zap.String("userID", userID), zap.Any("data", data), zap.Error(err))
+				u.Logger.ErrorAny("UpdateUserProfile", zap.String("action", "GetAllProjects"), zap.String("userID", userID), zap.Any("data", data), zap.Error(err))
 				continue
 			}
 
-			
 		}
 
 	}(*user)
 
-	u.Logger.LogAny("UpdateUserProfile", zap.String("userID", userID),zap.Any("input", data), zap.Any("user", user))
+	u.Logger.LogAny("UpdateUserProfile", zap.String("userID", userID), zap.Any("input", data), zap.Any("user", user))
 	if isUpdateWalletAddress {
-		u.NotifyWithChannel(os.Getenv("SLACK_USER_CHANNEL"), fmt.Sprintf("[User BTC wallet address has been updated][User %s][%s]", helpers.CreateProfileLink(user.WalletAddress, user.DisplayName), user.WalletAddress),"", fmt.Sprintf("BTC wallet address was changed from %s to %s", oldBtcAdress,  *data.WalletAddressBTC))
+		u.NotifyWithChannel(os.Getenv("SLACK_USER_CHANNEL"), fmt.Sprintf("[User BTC wallet address has been updated][User %s][%s]", helpers.CreateProfileLink(user.WalletAddress, user.DisplayName), user.WalletAddress), "", fmt.Sprintf("BTC wallet address was changed from %s to %s", oldBtcAdress, *data.WalletAddressBTC))
 	}
 
 	return user, nil
