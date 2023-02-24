622b443f52a5945e64553a182c1ded85ed4ea4b7
kenq79 <ken.q79@generative.xyz>
2023-02-24 14:28:03 +0700

    uopdat

--- internal/usecase/projects.go
+++ internal/usecase/projects.go
@@ -209,7 +209,7 @@ func (u Usecase) CreateBTCProject(req structure.CreateBtcProjectReq) (*entity.Pr
 		}
 	}
 
-	u.NotifyWithChannel(os.Getenv("SLACK_PROJECT_CHANNEL_ID"), fmt.Sprintf("[Project is created][project %s]", helpers.CreateProjectLink(pe.TokenID, pe.Name)), fmt.Sprintf("TraceID: %s", pe.TraceID), fmt.Sprintf("Project %s has been created by user %s", helpers.CreateProjectLink(pe.TokenID, pe.Name), helpers.CreateProfileLink(pe.ContractAddress, pe.CreatorName) ))
+	u.NotifyWithChannel(os.Getenv("SLACK_PROJECT_CHANNEL_ID"), fmt.Sprintf("[Project is created][project %s]", helpers.CreateProjectLink(pe.TokenID, pe.Name)), fmt.Sprintf("TraceID: %s", pe.TraceID), fmt.Sprintf("Project %s has been created by user %s", helpers.CreateProjectLink(pe.TokenID, pe.Name), helpers.CreateProfileLink(pe.ContractAddress, pe.CreatorName)))
 	u.NotifyCreateNewProjectToDiscord(pe, creatorAddrr)
 
 	return pe, nil
@@ -485,6 +485,13 @@ func (u Usecase) ReportProject(tokenId, iWalletAddress, originalLink string) (*e
 		return nil, err
 	}
 	u.Logger.Info("updated", updated)
+
+	u.NotifyWithChannel(
+		os.Getenv("SLACK_PROJECT_CHANNEL_ID"),
+		fmt.Sprintf("[Project is reported][projectID %s]", p.TokenID),
+		fmt.Sprintf("TraceID: %s", p.TraceID),
+		fmt.Sprintf("Project %s has been report by user %s - original link: %s", p.Name, iWalletAddress, originalLink),
+	)
 
 	return p, nil
 }
@@ -1075,7 +1082,7 @@ func (u Usecase) UnzipProjectFile(zipPayload *structure.ProjectUnzipPayload) (*e
 		u.Logger.Error("http.Get", err.Error(), err)
 		return nil, err
 	}
-	
+
 	nftTokenURI := make(map[string]interface{})
 	nftTokenURI["name"] = pe.Name
 	nftTokenURI["description"] = pe.Description
@@ -1083,7 +1090,6 @@ func (u Usecase) UnzipProjectFile(zipPayload *structure.ProjectUnzipPayload) (*e
 	nftTokenURI["animation_url"] = ""
 	nftTokenURI["attributes"] = []string{}
 
-	
 	images := []string{}
 	zipLink := zipPayload.ZipLink
 
@@ -1155,7 +1161,7 @@ func (u Usecase) UnzipProjectFile(zipPayload *structure.ProjectUnzipPayload) (*e
 		return nil, err
 	}
 
-	u.NotifyWithChannel(os.Getenv("SLACK_PROJECT_CHANNEL_ID"), fmt.Sprintf("[Project images are Unzipped][project %s]", helpers.CreateProjectLink(pe.TokenID, pe.Name)),"", fmt.Sprintf("Project's images have been unzipped with %d files, zipLink: %s", len(pe.Images), helpers.CreateTokenImageLink(zipLink)))
+	u.NotifyWithChannel(os.Getenv("SLACK_PROJECT_CHANNEL_ID"), fmt.Sprintf("[Project images are Unzipped][project %s]", helpers.CreateProjectLink(pe.TokenID, pe.Name)), "", fmt.Sprintf("Project's images have been unzipped with %d files, zipLink: %s", len(pe.Images), helpers.CreateTokenImageLink(zipLink)))
 	u.Logger.LogAny("UnzipProjectFile", zap.Any("updated", updated), zap.Any("project", pe))
 	return pe, nil
 }
@@ -1290,23 +1296,23 @@ func (u Usecase) CreateProjectFromCollectionMeta(meta entity.CollectionMeta) (*e
 }
 
 type Volumes struct {
-	Items []Volume `json:"items"`
-	TotalBTC float64 `json:"totalAmountBTC"`
-	TotalETH float64 `json:"totalAmountETH"`
+	Items    []Volume `json:"items"`
+	TotalBTC float64  `json:"totalAmountBTC"`
+	TotalETH float64  `json:"totalAmountETH"`
 }
 
 type Volume struct {
 	ProjectID string `json:"projectID"`
-	PayType string `json:"payType"`
-	Amount string `json:"amount"`
+	PayType   string `json:"payType"`
+	Amount    string `json:"amount"`
 }
 
 func (u Usecase) CreatorVolume(creatorAddr string) (interface{}, error) {
 	u.Logger.LogAny("CollectorVolume", zap.String("creatorAddr", creatorAddr))
-	
+
 	p, err := u.Repo.GetAllProjects(entity.FilterProjects{WalletAddress: &creatorAddr})
 	if err != nil {
-		u.Logger.ErrorAny("CollectorVolume", zap.String("creatorAddr", creatorAddr), zap.Any("err", err) )
+		u.Logger.ErrorAny("CollectorVolume", zap.String("creatorAddr", creatorAddr), zap.Any("err", err))
 	}
 
 	pIDs := []string{}
@@ -1317,15 +1323,15 @@ func (u Usecase) CreatorVolume(creatorAddr string) (interface{}, error) {
 
 	data, err := u.Repo.VolumeByProjectIDs(pIDs, entity.BTCWalletAddress{}.TableName())
 	if err != nil {
-		u.Logger.ErrorAny("CollectorVolume", zap.String("volumeByProjectIDs", creatorAddr), zap.Any("err", err) )
+		u.Logger.ErrorAny("CollectorVolume", zap.String("volumeByProjectIDs", creatorAddr), zap.Any("err", err))
 	}
 
 	resp := Volumes{}
-	for _, item := range  data.Items {
+	for _, item := range data.Items {
 		tmp := Volume{
 			ProjectID: item.ID.ProjectID,
-			PayType: item.ID.Paytype,
-			Amount: fmt.Sprintf("%d", int(item.Amount)),
+			PayType:   item.ID.Paytype,
+			Amount:    fmt.Sprintf("%d", int(item.Amount)),
 		}
 		resp.Items = append(resp.Items, tmp)
 	}
