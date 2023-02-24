758111d9efd776b83e1edeb8274c5bd3e60cb324
0xBahamoot <lam@incognito.org>
2023-02-24 14:09:08 +0700

    fix build

--- internal/delivery/http/wallet.go
+++ internal/delivery/http/wallet.go
@@ -1,35 +1,33 @@
 package http
 
 import (
-	"encoding/json"
 	"errors"
 	"net/http"
 
-	"rederinghub.io/internal/delivery/http/request"
 	"rederinghub.io/internal/delivery/http/response"
 	"rederinghub.io/utils"
 )
 
-func (h *httpDelivery) inscriptionByOutput(w http.ResponseWriter, r *http.Request) {
+// func (h *httpDelivery) inscriptionByOutput(w http.ResponseWriter, r *http.Request) {
 
-	var reqBody request.InscriptionByOutput
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&reqBody)
-	if err != nil {
-		h.Logger.Error("httpDelivery.inscriptionByOutput.Decode", err.Error(), err)
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
-		return
-	}
+// 	var reqBody request.InscriptionByOutput
+// 	decoder := json.NewDecoder(r.Body)
+// 	err := decoder.Decode(&reqBody)
+// 	if err != nil {
+// 		h.Logger.Error("httpDelivery.inscriptionByOutput.Decode", err.Error(), err)
+// 		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
+// 		return
+// 	}
 
-	result, _, err := h.Usecase.InscriptionsByOutputs(reqBody.Outputs)
-	if err != nil {
-		h.Logger.Error("httpDelivery.inscriptionByOutput.Usecase.InscriptionsByOutputs", err.Error(), err)
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
-		return
-	}
+// 	result, _, _, err := h.Usecase.InscriptionsByOutputs(reqBody.Outputs)
+// 	if err != nil {
+// 		h.Logger.Error("httpDelivery.inscriptionByOutput.Usecase.InscriptionsByOutputs", err.Error(), err)
+// 		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
+// 		return
+// 	}
 
-	h.Response.RespondSuccess(w, http.StatusOK, response.Success, result, "")
-}
+// 	h.Response.RespondSuccess(w, http.StatusOK, response.Success, result, "")
+// }
 
 func (h *httpDelivery) walletInfo(w http.ResponseWriter, r *http.Request) {
 	address := r.URL.Query().Get("address")
