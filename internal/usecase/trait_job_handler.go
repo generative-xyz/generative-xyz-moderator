89b8ace1c1e0e509bf9f1a6b025f24c2bbb0e5f5
Neraxis <neraxis@generative.xyz>
2023-02-27 19:55:23 +0700

    add cronjob to update trait values

---
+++ internal/usecase/trait_job_handler.go
@@ -0,0 +1,25 @@
+package usecase
+
+import "rederinghub.io/internal/usecase/structure"
+
+func (u Usecase) SyncTraitStats() error {
+	allTokens, err := u.Repo.GetAllTokensHasTraitSeletedFields()
+	if err != nil {
+		return err
+	}
+	projectSet := map[string]bool{}
+	for _, token := range allTokens {
+		projectSet[token.ProjectID] = true
+	}
+	for projectID, _ := range projectSet {
+		if projectID != "1000201" {
+			continue
+		}
+		_, traitStats, err := u.GetUpdatedProjectStats(structure.GetProjectReq{TokenID: projectID, ContractAddr: "0x0000000000000000000000000000000000000000"})
+		if err != nil {
+			return err
+		}
+		u.Repo.UpdateProjectTraitStats(projectID, traitStats)
+	}
+	return nil
+}
