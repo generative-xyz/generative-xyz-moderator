666e99b583a5dbb0cd055a5af60b8cc897ae9b35
Genesis Doge <doge666@generative.xyz>
2023-03-01 09:25:06 +0700

    update CreateInscribeBTC

--- internal/delivery/http/inscribe_btc.go
+++ internal/delivery/http/inscribe_btc.go
@@ -30,68 +30,54 @@ import (
 // @Router /inscribe/receive-address [POST]
 // @Security ApiKeyAuth
 func (h *httpDelivery) btcCreateInscribeBTC(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	userUuid := ctx.Value(utils.SIGNED_USER_ID).(string)
-	var reqBody request.CreateInscribeBtcReq
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&reqBody)
-	if err != nil {
-		h.Logger.Error("httpDelivery.btcCreateInscribeBTC.Decode", err.Error(), err)
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
-		return
-	}
+	response.NewRESTHandlerTemplate(
+		func(ctx context.Context, r *http.Request, vars map[string]string) (interface{}, error) {
+			userUuid := ctx.Value(utils.SIGNED_USER_ID).(string)
+			var reqBody request.CreateInscribeBtcReq
+			err := json.NewDecoder(r.Body).Decode(&reqBody)
+			if err != nil {
+				return nil, err
+			}
+			reqUsecase := &structure.InscribeBtcReceiveAddrRespReq{}
+			err = copier.Copy(reqUsecase, reqBody)
+			if err != nil {
+				return nil, err
+			}
 
-	reqUsecase := &structure.InscribeBtcReceiveAddrRespReq{}
-	err = copier.Copy(reqUsecase, reqBody)
-	if err != nil {
-		h.Logger.Error("copier.Copy", err.Error(), err)
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
-		return
-	}
+			if len(reqUsecase.FileName) == 0 {
+				return nil, errors.New("Filename is required")
+			}
 
-	if len(reqUsecase.FileName) == 0 {
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, errors.New("Filename is required"))
-		return
-	}
+			if len(reqUsecase.WalletAddress) == 0 {
+				return nil, errors.New("WalletAddress is required")
+			}
 
-	if len(reqUsecase.WalletAddress) == 0 {
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, errors.New("WalletAddress is required"))
-		return
-	}
+			if ok, _ := btc.ValidateAddress("btc", reqUsecase.WalletAddress); !ok {
+				return nil, errors.New("WalletAddress is invalid")
+			}
 
-	if ok, _ := btc.ValidateAddress("btc", reqUsecase.WalletAddress); !ok {
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, errors.New("WalletAddress is invalid"))
-		return
-	}
+			if reqUsecase.FeeRate != 15 && reqUsecase.FeeRate != 20 && reqUsecase.FeeRate != 25 {
+				return nil, errors.New("fee rate is invalid")
+			}
 
-	if reqUsecase.FeeRate != 15 && reqUsecase.FeeRate != 20 && reqUsecase.FeeRate != 25 {
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, errors.New("fee rate is invalid"))
-		return
-	}
-
-	if len(reqUsecase.File) == 0 {
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, errors.New("file is invalid"))
-		return
-	}
-	reqUsecase.SetFields(
-		reqUsecase.WithUserUuid(userUuid),
-	)
-	btcWallet, err := h.Usecase.CreateInscribeBTC(*reqUsecase)
-	if err != nil {
-		h.Logger.Error("h.Usecase.btcCreateInscribeBTC", err.Error(), err)
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
-		return
-	}
-
-	logger.AtLog.Logger.Info("btcCreateInscribeBTC", zap.Any("raw_data", btcWallet))
-	resp, err := h.inscribeBtcCreatedRespResp(btcWallet)
-	if err != nil {
-		h.Logger.Error(" h.proposalToResp", err.Error(), err)
-		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
-		return
-	}
-
-	h.Response.RespondSuccess(w, http.StatusOK, response.Success, resp, "")
+			if len(reqUsecase.File) == 0 {
+				return nil, errors.New("file is invalid")
+			}
+			reqUsecase.SetFields(
+				reqUsecase.WithUserUuid(userUuid),
+			)
+			btcWallet, err := h.Usecase.CreateInscribeBTC(ctx, *reqUsecase)
+			if err != nil {
+				logger.AtLog.Logger.Error("CreateInscribeBTC failed",
+					zap.Any("payload", reqBody),
+					zap.Error(err),
+				)
+				return nil, err
+			}
+			logger.AtLog.Logger.Info("CreateInscribeBTC successfully", zap.Any("response", btcWallet))
+			return h.inscribeBtcCreatedRespResp(btcWallet)
+		},
+	).ServeHTTP(w, r)
 }
 
 func (h *httpDelivery) inscribeBtcCreatedRespResp(input *entity.InscribeBTC) (*response.InscribeBtcResp, error) {
