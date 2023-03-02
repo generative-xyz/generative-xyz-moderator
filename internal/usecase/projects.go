dd3ebd76bbe9bcc451c21ca2be8fcff2fd6b7dd0
Genesis Doge <doge666@generative.xyz>
2023-03-02 15:20:18 +0700

    update cal eth mint fee price project detail

--- internal/usecase/projects.go
+++ internal/usecase/projects.go
@@ -947,7 +947,7 @@ func (u Usecase) GetProjectDetail(req structure.GetProjectDetailMessageReq) (*en
 		calNetworkFee := u.networkFeeBySize(int64(c.MaxFileSize / 4))
 		if calNetworkFee > 0 {
 			networkFeeInt = calNetworkFee
-			c.NetworkFee = fmt.Sprint("%d", networkFeeInt)
+			c.NetworkFee = fmt.Sprintf("%d", networkFeeInt)
 
 		}
 	}
