88e03fec819f3a1c575bf1ed8dacf39e7de6f166
Genesis Doge <doge666@generative.xyz>
2023-03-06 15:03:12 +0700

    fix create token uri

--- internal/usecase/structure/inscribe_btc.go
+++ internal/usecase/structure/inscribe_btc.go
@@ -1,14 +1,15 @@
 package structure
 
 type InscribeBtcReceiveAddrRespReq struct {
-	WalletAddress string `json:"walletAddress"`
-	Name          string `json:"name"`
-	FileName      string `json:"fileName"`
-	File          string `json:"file"`
-	FeeRate       int32  `json:"fee_rate"`
-	UserUuid      string `json:"-"`
-	TokenAddress  string `json:"tokenAddress"`
-	TokenId       string `json:"tokenId"`
+	WalletAddress     string `json:"walletAddress"`
+	Name              string `json:"name"`
+	FileName          string `json:"fileName"`
+	File              string `json:"file"`
+	FeeRate           int32  `json:"fee_rate"`
+	UserUuid          string `json:"-"`
+	UserWallerAddress string `json:"-"`
+	TokenAddress      string `json:"tokenAddress"`
+	TokenId           string `json:"tokenId"`
 }
 
 func (s InscribeBtcReceiveAddrRespReq) NeedVerifyAuthentic() bool {
@@ -23,5 +24,10 @@ func (s *InscribeBtcReceiveAddrRespReq) SetFields(fns ...func(*InscribeBtcReceiv
 func (s InscribeBtcReceiveAddrRespReq) WithUserUuid(userUuid string) func(*InscribeBtcReceiveAddrRespReq) {
 	return func(ibrarr *InscribeBtcReceiveAddrRespReq) {
 		ibrarr.UserUuid = userUuid
+	}
+}
+func (s InscribeBtcReceiveAddrRespReq) WithUserWallerAddress(UserWallerAddress string) func(*InscribeBtcReceiveAddrRespReq) {
+	return func(ibrarr *InscribeBtcReceiveAddrRespReq) {
+		ibrarr.UserWallerAddress = UserWallerAddress
 	}
 }
