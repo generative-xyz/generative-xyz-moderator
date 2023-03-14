1d530a43a58463fb2c96c5044157f0884209a14f
goku <chihuy@autonomous.nyc>
2023-03-14 12:14:18 +0700

    update

--- internal/delivery/http/request/dao_project.go
+++ internal/delivery/http/request/dao_project.go
@@ -6,6 +6,7 @@ type ListDaoProjectRequest struct {
 	entity.Pagination
 	Status  *int64  `query:"status"`
 	Keyword *string `query:"keyword"`
+	Id      *string `query:"-"`
 }
 type CreateDaoProjectRequest struct {
 	ProjectId string `json:"project_id"`
