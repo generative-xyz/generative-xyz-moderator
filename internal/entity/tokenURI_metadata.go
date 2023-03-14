99d8671aedf079ab574b4658566a38629a091be4
genesis-0000 <thaibao@autonomous.nyc>
2023-03-14 13:55:57 +0700

    - hotfix

--- internal/entity/tokenURI_metadata.go
+++ internal/entity/tokenURI_metadata.go
@@ -7,38 +7,39 @@ import (
 )
 
 type TokenMetadataStatus string
+
 const (
 	METADATA_UPLOADED TokenHistoryType = "balance"
-	METADATA_UPDATED TokenHistoryType = "mint"
+	METADATA_UPDATED  TokenHistoryType = "mint"
 )
 
 type FilterTokenUriMetadata struct {
 	BaseFilters
 	ProjectID []string
-	TokenID []string
+	TokenID   []string
 }
 
 type TokenUriMetadata struct {
-	BaseEntity`bson:",inline"`
-	ProjectID string `bson:"projectID"`
-	UploadedFile string `bson:"uploadedFile"`
-	Content []TokenTraits `bson:"content"`
+	BaseEntity   `bson:",inline"`
+	ProjectID    string        `bson:"projectID"`
+	UploadedFile string        `bson:"uploadedFile"`
+	Content      []TokenTraits `bson:"content"`
 }
 
 type TokenTraits struct {
-	ID string `bson:"id" json:"id"`
-	Atrributes []TraitAttribute `bson:"atrributes" json:"atrributes"`
+	ID         string           `bson:"id" json:"id"`
+	Atrributes []TraitAttribute `bson:"attributes" json:"attributes"`
 }
 
 type TraitAttribute struct {
 	TraitType string `bson:"trait_type" json:"trait_type"`
-	Value string `bson:"value"  json:"value"`
+	Value     string `bson:"value"  json:"value"`
 }
 
-func (u TokenUriMetadata) TableName() string { 
+func (u TokenUriMetadata) TableName() string {
 	return utils.COLLECTION_TOKEN_URI_METADATA
 }
 
-func (u TokenUriMetadata) ToBson()  (*bson.D, error) { 
+func (u TokenUriMetadata) ToBson() (*bson.D, error) {
 	return helpers.ToDoc(u)
 }
