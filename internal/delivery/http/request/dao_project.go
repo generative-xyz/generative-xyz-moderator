450662a282a8da47f74be6bbb65cf2ef7d83da92
Genesis Doge <doge666@generative.xyz>
2023-03-14 13:53:24 +0700

    update

--- internal/delivery/http/request/dao_project.go
+++ internal/delivery/http/request/dao_project.go
@@ -1,6 +1,9 @@
 package request
 
-import "rederinghub.io/internal/entity"
+import (
+	"rederinghub.io/internal/entity"
+	"rederinghub.io/utils/constants/dao_project_voted"
+)
 
 type ListDaoProjectRequest struct {
 	entity.Pagination
@@ -11,4 +14,9 @@ type ListDaoProjectRequest struct {
 type CreateDaoProjectRequest struct {
 	ProjectId string `json:"project_id"`
 	CreatedBy string `json:"-"`
+}
+
+type VoteDaoProjectRequest struct {
+	Status        dao_project_voted.Status `json:"int64"`
+	WalletAddress string                   `json:"-"`
 }
