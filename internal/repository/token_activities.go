113348366c42b22e950156c102ee5a69bddf2627
Neraxis <neraxis@generative.xyz>
2023-03-15 11:48:02 +0700

    fix token activities schema

--- internal/repository/token_activities.go
+++ internal/repository/token_activities.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"go.mongodb.org/mongo-driver/bson"
 	"rederinghub.io/internal/entity"
 )
 
@@ -10,4 +11,40 @@ func (r Repository) InsertTokenActivity(data *entity.TokenActivity) error {
 		return err
 	}
 	return nil
+}
+
+func (r Repository) buildFilterTokenActivities(filter entity.FilterTokenActivities) bson.M {
+	f := bson.M{}
+	if filter.InscriptionID != nil {
+		f["inscription_id"] = *filter.InscriptionID
+	}
+	if filter.ProjectID != nil {
+		f["project_id"] = *filter.ProjectID
+	}
+	return f
+}
+
+func (r Repository) GetTokenActivities(filter entity.FilterTokenActivities) (*entity.Pagination, error) {
+	activities := []entity.TokenActivity{}
+	resp := &entity.Pagination{}
+	f := r.buildFilterTokenActivities(filter)
+	if filter.SortBy == "" {
+		filter.SortBy = "time"
+		filter.Sort = entity.SORT_DESC
+	}
+	s := []Sort{
+		{SortBy: filter.SortBy, Sort: filter.Sort},
+		{SortBy: "uuid", Sort: entity.SORT_DESC},
+	}
+	t, err := r.Paginate(entity.TokenUri{}.TableName(), filter.Page, filter.Limit, f, bson.D{}, s, &activities)
+	if err != nil {
+		return nil, err
+	}
+
+	resp.Result = activities
+	resp.Page = t.Pagination.Page
+	resp.Total = t.Pagination.Total
+	resp.PageSize = filter.Limit
+	//resp.PageSize = filter.Limit
+	return resp, nil
 }
