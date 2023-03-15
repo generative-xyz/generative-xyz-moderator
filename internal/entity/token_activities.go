4d18479e878b13b7b931f912efc900ebcece8afe
Neraxis <neraxis@generative.xyz>
2023-03-15 10:20:06 +0700

    add job create token activities

--- internal/entity/token_activities.go
+++ internal/entity/token_activities.go
@@ -1,6 +1,12 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"rederinghub.io/utils"
+	"rederinghub.io/utils/helpers"
+)
 
 type TokenActivityType int
 const (
@@ -11,12 +17,23 @@ const (
 )
 
 type TokenActivity struct {
-	Type TokenActivityType
-	Title string
-	UserAAddress string
-	UserA *Users
-	UserBAddress string
-	UserB *Users
-	Amount int64
-	Time  *time.Time
+	BaseEntity    `bson:"base_entity" json:"base_entity"`
+	Type          TokenActivityType `bson:"type" json:"type"`
+	Title         string            `bson:"title" json:"title"`
+	UserAAddress  string            `bson:"user_a_address" json:"user_a_address"`
+	UserA         *Users            `bson:"-" json:"user_a"`
+	UserBAddress  string            `bson:"user_b_address" json:"user_b_address"`
+	UserB         *Users            `bson:"-" json:"user_b"`
+	Amount        int64             `bson:"amount" json:"amount"`
+	Time          *time.Time        `bson:"time" json:"time"`
+	InscriptionID string            `bson:"inscription_id" json:"inscription_id"`
+	ProjectID     string            `bson:"project_id" json:"project_id"`
+}
+
+func (u TokenActivity) TableName() string { 
+	return utils.COLLECTION_TOKEN_ACTIVITY
+}
+
+func (u TokenActivity) ToBson()  (*bson.D, error) { 
+	return helpers.ToDoc(u)
 }
