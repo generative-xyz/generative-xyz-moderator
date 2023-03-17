09ffede8eadcc37733295f1c725da500e4083af5
goku <chihuy@autonomous.nyc>
2023-03-17 10:11:24 +0700

    update

--- internal/delivery/http/middleware/middleware.go
+++ internal/delivery/http/middleware/middleware.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"rederinghub.io/internal/delivery/http/response"
-	_httpResponse "rederinghub.io/internal/delivery/http/response"
 	"rederinghub.io/internal/usecase"
 	"rederinghub.io/utils"
 	"rederinghub.io/utils/global"
@@ -20,16 +19,14 @@ import (
 
 type IMiddleware interface {
 	LoggingMiddleware(next http.Handler) http.Handler
-	VerifyEmail(next http.Handler) http.Handler
 	AccessToken(next http.Handler) http.Handler
-	UserToken(next http.Handler) http.Handler
-	AuthorizeFunc(next http.Handler) http.Handler
+	AuthorizationFunc(next http.Handler) http.Handler
 }
 
 type middleware struct {
 	log              logger.Ilogger
 	usecase          usecase.Usecase
-	response         _httpResponse.IHttpResponse
+	response         response.IHttpResponse
 	cache            redis.IRedisCache
 	cacheAuthService redis.IRedisCache
 }
@@ -38,7 +35,7 @@ func NewMiddleware(uc usecase.Usecase, g *global.Global) *middleware {
 	m := new(middleware)
 	m.log = g.Logger
 	m.usecase = uc
-	m.response = _httpResponse.NewHttpResponse()
+	m.response = response.NewHttpResponse()
 	m.cache = g.Cache
 	m.cacheAuthService = g.CacheAuthService
 	return m
@@ -89,6 +86,7 @@ func (rw *responseWriter) WriteHeader(code int) {
 	rw.wroteHeader = true
 }
 
+// Authenticate
 func (m *middleware) AccessToken(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 
@@ -126,49 +124,9 @@ func (m *middleware) AccessToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
-func (m *middleware) UserToken(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-
-		ctx := r.Context()
-		token := r.Header.Get(utils.AUTH_TOKEN)
-		if token != "" {
-			token = helpers.ReplaceToken(token)
-			p, err := m.usecase.ValidateAccessToken(token)
-			if err == nil {
-				m.log.Info("profile", p)
-				//m.log.Info("signedDetail", signedDetail)
-				m.cache.SetData(helpers.GenerateCachedProfileKey(token), p)
-				m.cache.SetStringData(helpers.GenerateUserKey(token), p.Uid)
-
-				ctx = context.WithValue(ctx, utils.AUTH_TOKEN, token)
-				ctx = context.WithValue(ctx, utils.SIGNED_WALLET_ADDRESS, p.WalletAddress)
-				//ctx = context.WithValue(ctx, utils.SIGNED_EMAIL, p.Email)
-				ctx = context.WithValue(ctx, utils.SIGNED_USER_ID, p.Uid)
-			}
-
-		}
-
-		wrapped := wrapResponseWriter(w)
-		next.ServeHTTP(wrapped, r.WithContext(ctx))
-	}
-
-	return http.HandlerFunc(fn)
-}
-
-//TODO - Is the inserted email belong to the correct ID.
-func (m *middleware) VerifyEmail(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-
-		wrapped := wrapResponseWriter(w)
-		next.ServeHTTP(wrapped, r)
-	}
-
-	return http.HandlerFunc(fn)
-}
-
 // Just set SIGNED_WALLET_ADDRESS, SIGNED_USER_ID to context if have
-// Authorize not Authenticate
-func (m *middleware) AuthorizeFunc(next http.Handler) http.Handler {
+// Authorization
+func (m *middleware) AuthorizationFunc(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		token := helpers.ReplaceToken(r.Header.Get(utils.AUTH_TOKEN))
 		if token == "" {
@@ -181,6 +139,7 @@ func (m *middleware) AuthorizeFunc(next http.Handler) http.Handler {
 			return
 		}
 		ctx := r.Context()
+		ctx = context.WithValue(ctx, utils.AUTH_TOKEN, token)
 		ctx = context.WithValue(ctx, utils.SIGNED_WALLET_ADDRESS, p.WalletAddress)
 		ctx = context.WithValue(ctx, utils.SIGNED_USER_ID, p.Uid)
 		wrapped := wrapResponseWriter(w)
