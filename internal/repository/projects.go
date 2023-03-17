ad1d5367cd947c83b4d89fecb6c3e77b05eb9985
tri luong <tri@autonomous.nyc>
2023-03-17 16:27:34 +0700

    hot-fix

--- internal/repository/projects.go
+++ internal/repository/projects.go
@@ -283,11 +283,11 @@ func (r Repository) GetProjects(filter entity.FilterProjects) (*entity.Paginatio
 	resp := &entity.Pagination{}
 	f := r.FilterProjects(filter)
 
-	query := `{ "$where": "this.limitSupply > this.index + this.indexReverse " }`
-	err := json.Unmarshal([]byte(query), &f)
-	if err != nil {
-		return nil, err
-	}
+	// query := `{ "$where": "this.limitSupply > this.index + this.indexReverse " }`
+	// err := json.Unmarshal([]byte(query), &f)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
 	var s []Sort
 	if filter.SortBy == "" {
