b11fb29ab296a0db82188c3c0674d8b71dd1d294
Genesis Doge <doge666@generative.xyz>
2023-03-20 16:09:50 +0700

    Add chart data for tokenURI

--- internal/usecase/structure/dex_volume_inscription_filter.go
+++ internal/usecase/structure/dex_volume_inscription_filter.go
@@ -11,13 +11,29 @@ type AggerateChartForProject struct {
 	ToDate *time.Time `json:"toDate"`
 }
 
-type AggragetedInscription struct {
+type AggerateChartForToken struct {
+	TokenID *string `json:"tokenID"`
+	FromDate *time.Time `json:"fromDate"`
+	ToDate *time.Time `json:"toDate"`
+}
+
+type AggragetedCollection struct {
 	ProjectID string `json:"projectID"` 
 	ProjectName string `json:"projectName"` 
 	Timestamp string `json:"timestamp"` 
 	Amount int64 `json:"amount"`
 }
 
-type AggragetedInscriptionVolumnResp struct {
-	Volumns []AggragetedInscription `json:"volumns"` 
+type AggragetedTokenURI struct {
+	TokenID string `json:"tokenID"` 
+	Timestamp string `json:"timestamp"` 
+	Amount int64 `json:"amount"`
+}
+
+type AggragetedCollectionVolumnResp struct {
+	Volumns []AggragetedCollection `json:"volumns"` 
+}
+
+type AggragetedTokenVolumnResp struct {
+	Volumns []AggragetedTokenURI `json:"volumns"` 
 }
