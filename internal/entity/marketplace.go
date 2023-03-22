7d8bd0eaf57dcc638bc4dd22ac66f9704e4a66d7
kenq79 <ken.q79@generative.xyz>
2023-03-22 11:36:21 +0700

    update code

--- internal/entity/marketplace.go
+++ internal/entity/marketplace.go
@@ -1,17 +1,19 @@
 package entity
 
 type ItemListing struct {
-	InscriptionId     string         `json:"inscription_id"`
-	VolumeOneHour     *VolumneObject `json:"volumeOneHour"`
-	VolumeOneDay      *VolumneObject `json:"volumeOneDay"`
-	VolumeOneWeek     *VolumneObject `json:"volumeOneWeek"`
-	SellerAddress     string         `json:"sellerAddress"`
-	SellerDisplayName string         `json:"sellerDisplayName"`
-	BuyerAddress      string         `json:"buyerAddress"`
-	BuyerDisplayName  string         `json:"buyerDisplayName"`
-	Name              string         `json:"name"`
-	Image             string         `json:"image"`
-	ContentType       string         `json:"contentType"`
+	InscriptionId         string         `json:"inscription_id"`
+	VolumeOneHour         *VolumneObject `json:"volumeOneHour"`
+	VolumeOneDay          *VolumneObject `json:"volumeOneDay"`
+	VolumeOneWeek         *VolumneObject `json:"volumeOneWeek"`
+	SellerAddress         string         `json:"sellerAddress"`
+	SellerDisplayName     string         `json:"sellerDisplayName"`
+	BuyerAddress          string         `json:"buyerAddress"`
+	BuyerDisplayName      string         `json:"buyerDisplayName"`
+	Name                  string         `json:"name"`
+	Image                 string         `json:"image"`
+	ContentType           string         `json:"contentType"`
+	OrderInscriptionIndex float64        `json:"orderInscriptionIndex"`
+	InscriptionIndex      float64        `json:"inscriptionIndex"`
 }
 
 type VolumneObject struct {
