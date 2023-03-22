31811ffe35c9f13cea9245354f2f7c2583f1198c
Genesis Doge <doge666@generative.xyz>
2023-03-22 17:54:55 +0700

    update router

--- internal/delivery/http/routes.go
+++ internal/delivery/http/routes.go
@@ -182,11 +182,11 @@ func (h *httpDelivery) RegisterV1Routes() {
 	inscribe.HandleFunc("/nft-detail/{ID}", h.btcDetailInscribeBTC).Methods("GET")
 	inscribe.HandleFunc("/retry/{ID}", h.btcRetryInscribeBTC).Methods("POST")
 	inscribe.HandleFunc("/info/{ID}", h.getInscribeInfo).Methods("GET")
+	inscribe.HandleFunc("/compress-image", h.compressImage).Methods("POST")
 
 	inscribeAuth := inscribe.PathPrefix("/auth").Subrouter()
 	inscribeAuth.Use(h.MiddleWare.AccessToken)
 	inscribeAuth.HandleFunc("/receive-address", h.btcCreateInscribeBTC).Methods("POST")
-	inscribeAuth.HandleFunc("/compress-image", h.compressImage).Methods("POST")
 
 	tokenMoralis := api.PathPrefix("/token-moralis").Subrouter()
 	tokenMoralis.Use(h.MiddleWare.AccessToken)
