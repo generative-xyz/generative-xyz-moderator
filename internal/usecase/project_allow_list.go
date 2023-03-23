570e5b9dc9426547fc4f9cb02fa412282674a56a
genesis-0000 <thaibao@autonomous.nyc>
2023-03-23 13:56:45 +0700

    - fix slack

--- internal/usecase/project_allow_list.go
+++ internal/usecase/project_allow_list.go
@@ -56,7 +56,7 @@ func (u Usecase) CreateProjectAllowList(req structure.CreateProjectAllowListReq)
 	//SLACK_ALLOW_LIST_CHANNEL
 	go func(u Usecase, userAddress string, user entity.Users, p entity.Projects, allowBy entity.AllowedByType) {
 		count, _ := u.Repo.GetProjectAllowListTotal(p.TokenID)
-		u.NotifyWithChannel(os.Getenv("SLACK_ALLOW_LIST_CHANNEL"), fmt.Sprintf("[Allowlist][User %s]", helpers.CreateProfileLink(user.WalletAddress, user.DisplayName)), userAddress, fmt.Sprintf("%s registered to  %s's allowlist allowBy: %s TOTAL: %d", helpers.CreateProfileLink(user.WalletAddress, user.DisplayName), helpers.CreateProjectLink(p.TokenID, p.Name), allowedBy, count))
+		u.NotifyWithChannel(os.Getenv("SLACK_ALLOW_LIST_CHANNEL"), fmt.Sprintf("[Allowlist][User %s]", helpers.CreateProfileLink(user.WalletAddress, user.DisplayName)), userAddress, fmt.Sprintf("%s registered to  %s's allowlist allowBy: %s TOTAL: %d", helpers.CreateProfileLink(user.WalletAddressBTCTaproot, user.DisplayName), helpers.CreateProjectLink(p.TokenID, p.Name), allowedBy, count))
 	}(u, userAddress, *user, *p, allowedBy)
 	return pe, nil
 }
