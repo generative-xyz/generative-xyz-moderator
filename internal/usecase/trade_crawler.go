19f0ce3b924af7e5b8317d71693472524b3dabae
Neraxis <neraxis@generative.xyz>
2023-03-24 10:59:06 +0700

    fix JobCrawlTokenTxNotFromTokenUri

--- internal/usecase/trade_crawler.go
+++ internal/usecase/trade_crawler.go
@@ -384,7 +384,22 @@ func (u Usecase) JobCrawlTokenTxNotFromTokenUri() error {
 				u.Logger.Info("JobCrawlTokenTxNotFromTokenUri.InsertTokenTx", zap.Any("tokenTx", tokenTx))
 			}
 		}
-		u.Repo.UpdateGlobalActivity(LAST_CRAWL_INSCRIPTION_INDEX_KEY, to)
+		minOf := func (vars ...int) int {
+			min := vars[0]
+		
+			for _, i := range vars {
+				if min > i {
+					min = i
+				}
+			}
+		
+			return min
+		}
+		mxInscriptionIndex := 0
+		for _, inscription := range tx.Data.Result {
+			mxInscriptionIndex = minOf(inscription.Inscription.Number, mxInscriptionIndex)
+		}
+		u.Repo.UpdateGlobalActivity(LAST_CRAWL_INSCRIPTION_INDEX_KEY, mxInscriptionIndex)
 		time.Sleep(time.Second)
 	}
 	return nil
