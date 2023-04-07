692a9c17717f43ad8a1457faa7c9e67abbb01820
Genesis Doge <doge666@generative.xyz>
2023-04-08 04:08:58 +0700

    update fee min 4

--- internal/usecase/utils.go
+++ internal/usecase/utils.go
@@ -114,33 +114,50 @@ func (u Usecase) GetLevelFeeInfo(fileSize, customRate, mintPrice int64) (map[str
 		return nil, err
 	}
 
-	fmt.Println("fastestFee", feeRateFromChain.FastestFee)
-	fmt.Println("halfHourFee", feeRateFromChain.HalfHourFee)
-	fmt.Println("hourFee", feeRateFromChain.EconomyFee) // not use HourFee
+	fastestFee := feeRateFromChain.FastestFee
+	fasterFee := feeRateFromChain.HalfHourFee
+	ecoFee := feeRateFromChain.EconomyFee // not use HourFee
 
-	fastestMintInfo, err := u.calMintFeeInfo(mintPrice, fileSize, int64(feeRateFromChain.FastestFee), btcRate, ethRate)
+	fmt.Println("fastestFee", fastestFee)
+	fmt.Println("fasterFee", fasterFee)
+	fmt.Println("ecoFee", ecoFee)
+
+	min := 4 // todo: move config
+
+	// set mint 5 if fee < 5.
+	if fastestFee < min {
+		fastestFee = min
+	}
+	if fasterFee < min {
+		fasterFee = min
+	}
+	if ecoFee < min {
+		ecoFee = min
+	}
+
+	fastestMintInfo, err := u.calMintFeeInfo(mintPrice, fileSize, int64(fastestFee), btcRate, ethRate)
 	if err != nil {
 		return nil, err
 	}
-	fasterMintInfo, err := u.calMintFeeInfo(mintPrice, fileSize, int64(feeRateFromChain.HalfHourFee), btcRate, ethRate)
+	fasterMintInfo, err := u.calMintFeeInfo(mintPrice, fileSize, int64(fasterFee), btcRate, ethRate)
 	if err != nil {
 		return nil, err
 	}
-	economyMintInfo, err := u.calMintFeeInfo(mintPrice, fileSize, int64(feeRateFromChain.EconomyFee), btcRate, ethRate)
+	economyMintInfo, err := u.calMintFeeInfo(mintPrice, fileSize, int64(ecoFee), btcRate, ethRate)
 	if err != nil {
 		return nil, err
 	}
 
 	levelFeeFullInfo["fastest"] = FeeRateInfo{
-		FeeRate:     feeRateFromChain.FastestFee,
+		FeeRate:     fastestFee,
 		MintFeeInfo: fastestMintInfo,
 	}
 	levelFeeFullInfo["faster"] = FeeRateInfo{
-		FeeRate:     feeRateFromChain.HalfHourFee,
+		FeeRate:     fasterFee,
 		MintFeeInfo: fasterMintInfo,
 	}
 	levelFeeFullInfo["economy"] = FeeRateInfo{
-		FeeRate:     feeRateFromChain.EconomyFee,
+		FeeRate:     ecoFee,
 		MintFeeInfo: economyMintInfo,
 	}
 
