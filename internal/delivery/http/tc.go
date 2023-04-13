98d201f436bfd588b00e3db243d38b2c4ff5ef81
Hoang Phuong <phuong@autonomous.nyc>
2023-04-13 16:23:20 +0700

    add nfts-owner tc

--- internal/delivery/http/tc.go
+++ internal/delivery/http/tc.go
@@ -28,3 +28,22 @@ func (h *httpDelivery) getNftsByAddress(w http.ResponseWriter, r *http.Request) 
 
 	h.Response.RespondSuccess(w, http.StatusOK, response.Success, result, "")
 }
+
+func (h *httpDelivery) getNftsByAddressFromTokenUri(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	address := vars["address"]
+
+	if address == "" {
+		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, errors.New("address cannot be empty"))
+		return
+	}
+	result, err := h.Usecase.GetNftsByAddressFromTokenUri(address)
+	if err != nil {
+		logger.AtLog.Logger.Error("httpDelivery.walletInfo.Usecase.GetNftsByAddress", zap.Error(err))
+		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
+		return
+	}
+
+	h.Response.RespondSuccess(w, http.StatusOK, response.Success, result, "")
+}
