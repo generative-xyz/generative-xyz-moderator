cdce2e8446b98b71ec037f502ed2487755d8fcca
axe31803 <axe31803@generative.xyz>
2023-04-19 18:19:58 +0700

    Move hardware capture image into pubsub

--- internal/delivery/http/photographer.go
+++ internal/delivery/http/photographer.go
@@ -2,14 +2,10 @@ package http
 
 import (
 	"context"
-	"fmt"
 	"net/http"
-	"os"
 
-	"go.uber.org/zap"
 	"rederinghub.io/internal/delivery/http/request"
 	"rederinghub.io/internal/delivery/http/response"
-	"rederinghub.io/utils/logger"
 	_req "rederinghub.io/utils/request"
 )
 
@@ -31,20 +27,12 @@ func (h *httpDelivery) Capture(w http.ResponseWriter, r *http.Request) {
 			if err != nil {
 				return nil, err
 			}
-			url, err := h.Usecase.CaptureContent(req.ID, req.Url)
+			err = h.Usecase.PublishImageData(req)
 			if err != nil {
 				return nil, err
 			}
 
-			renderURL := fmt.Sprintf("%s/api/v1/device/%s/renderer-set-image", os.Getenv("RENDER_DOMAIN"), req.ID)
-			code, result, err := _req.PostToRenderer(renderURL, map[string]string{
-				"image_url": url,
-			})
-			logger.AtLog.Info("call to renderer-set-image ", zap.Error(err), zap.String("renderURL", renderURL), zap.String("device_id", req.ID), zap.Int("code", code), zap.String("response", string(result)))
-
-			return response.CaptureResponse{
-				ImageUrl: url,
-			}, nil
+			return req, nil
 
 		},
 	).ServeHTTP(w, r)
