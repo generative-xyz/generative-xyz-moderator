b2129ea4feb4e8f5f6fde03d84e51cf5b9fc4ed1
Genesis Doge <doge666@generative.xyz>
2023-04-20 16:55:31 +0700

    update

--- internal/delivery/http/request/faucetReq.go
+++ internal/delivery/http/request/faucetReq.go
@@ -4,4 +4,6 @@ type FaucetReq struct {
 	Url               string `json:"url"`
 	Address           string `json:"address"`
 	RecaptchaResponse string `json:"g-recaptcha-response"`
+	Type              string `json:"type"`
+	Txhash            string `json:"txhash"`
 }
