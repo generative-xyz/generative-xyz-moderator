d995e9ce5affeb6a3c503f9f96a250b84b67ca35
Phong Nguyen <phongnguyen@Phongs-MacBook-Pro.local>
2023-04-24 09:59:49 +0700

    load collection repositoris from env

--- internal/delivery/crontab_ordinal_collections/crontab.go
+++ internal/delivery/crontab_ordinal_collections/crontab.go
@@ -126,14 +126,20 @@ func (h ScronOrdinalCollectionHandler) StartServer() {
 	c := cron.New()
 	// cronjob to sync ordinals collection
 	c.AddFunc("0 */2 * * *", func() {
-		source := "https://github.com/ordinals-wallet/ordinals-collections.git"
+		source := os.Getenv("ORDINAL_COLLECTION_REPOSITORY")
+		if source == "" {
+			source = "https://github.com/ordinals-wallet/ordinals-collections.git"
+		}
 		err := h.crawlOrdinalCollection(source)
 		if err != nil {
 			logger.AtLog.Logger.Error("DispatchCron.EveryTwoHour.SyncOrdinalWalletCollections", zap.Error(err))
 		}
 	})
 	c.AddFunc("*/15 * * * *", func() {
-		source := "https://github.com/generative-xyz/ordinals-collections.git"
+		source := os.Getenv("GENERATIVE_COLLECTION_REPOSITORY")
+		if source == "" {
+			source = "https://github.com/generative-xyz/ordinals-collections.git"
+		}
 		err := h.crawlOrdinalCollection(source)
 		if err != nil {
 			logger.AtLog.Logger.Error("DispatchCron.EveryFifteenMinutes.SyncGenerativeCollections", zap.Error(err))
