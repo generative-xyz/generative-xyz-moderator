7925328904b946ca84321d704451d6d9379249ca
Genesis Doge <doge666@generative.xyz>
2023-04-25 16:18:37 +0700

    update faucet

--- utils/constants/faucet/constant.go
+++ utils/constants/faucet/constant.go
@@ -4,4 +4,5 @@ const (
 	NormalFaucetAmount   = 100000000000000000 // 0.1
 	BNSFaucetAmount      = 100000000000000000
 	ArtifactFaucetAmount = 100000000000000000
+	SpecialFaucetAmount  = 200000000000000000
 )
