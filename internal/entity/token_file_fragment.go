c32a81f84f31795a93a71984cb9ab24fe53641ea
Genesis Doge <doge666@generative.xyz>
2023-04-26 22:48:53 +0700

    add split and merge file funstion

---
+++ internal/entity/token_file_fragment.go
@@ -0,0 +1,31 @@
+package entity
+
+import (
+	"rederinghub.io/utils"
+	"time"
+)
+
+type FileFragmentStatus int
+
+const (
+	FileFragmentStatusCreated FileFragmentStatus = iota + 1
+	FileFragmentStatusProcessing
+	FileFragmentStatusDone
+	FileFragmentStatusError
+)
+
+type TokenFileFragment struct {
+	BaseEntity `bson:",inline" json:"base_entity"`
+	TokenId    string             `json:"token_id" bson:"token_id"`
+	FilePath   string             `json:"file_path" bson:"file_path"`
+	Sequence   int                `json:"sequence" bson:"sequence"`
+	Data       []byte             `json:"data" bson:"data"`
+	Status     FileFragmentStatus `json:"status" bson:"status"`
+	Note       string             `json:"note" bson:"note"`
+	UploadTime *time.Time         `json:"upload_time" bson:"upload_time"`
+	CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
+}
+
+func (m TokenFileFragment) TableName() string {
+	return utils.TOKEN_FILE_FRAGMENT
+}
