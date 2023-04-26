3e5f83978516c572c8d054941f26c6d374ca2532
Genesis Doge <doge666@generative.xyz>
2023-04-26 23:16:02 +0700

    done job

--- internal/repository/token_file_fragment.go
+++ internal/repository/token_file_fragment.go
@@ -19,6 +19,12 @@ type TokenFileFragmentFileter struct {
 	PageSize int
 }
 
+type TokenFragmentJobFilter struct {
+	Status   entity.TokenFragmentJobStatus
+	Page     int
+	PageSize int
+}
+
 func (r Repository) FindTokenFileFragment(ctx context.Context, tokenID string, sequence int) (*entity.TokenFileFragment, error) {
 	var file entity.TokenFileFragment
 	err := r.DB.Collection(file.TableName()).FindOne(ctx, bson.M{"token_id": tokenID, "sequence": sequence}).Decode(&file)
@@ -88,4 +94,73 @@ func (r Repository) FindTokenFileFragments(ctx context.Context, filter TokenFile
 		return nil, err
 	}
 	return result, nil
+}
+
+func (r Repository) CreateFragmentJob(ctx context.Context, job *entity.TokenFragmentJob) error {
+
+	if job.TokenId == "" {
+		return fmt.Errorf("token id is required")
+	}
+
+	if job.FilePath == "" {
+		return fmt.Errorf("file path is required")
+	}
+
+	now := time.Now()
+	id := primitive.NewObjectID()
+	job.CreatedAt = &now
+
+	job.BaseEntity = entity.BaseEntity{
+		ID:   id,
+		UUID: id.Hex(),
+	}
+
+	job.Status = entity.FragmentJobStatusPending
+	_, err := r.DB.Collection(job.TableName()).InsertOne(ctx, job)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r Repository) UpdateFragmentJobStatus(ctx context.Context, uuid string, status entity.TokenFragmentJobStatus, note string) error {
+	_, err := r.DB.Collection(utils.TOKEN_FILE_FRAGMENT_JOB).UpdateOne(ctx, bson.M{"uuid": uuid}, bson.M{"$set": bson.M{"status": status, "note": note}})
+	return err
+}
+
+func (r Repository) FindFragmentJobs(ctx context.Context, filter TokenFragmentJobFilter) ([]entity.TokenFragmentJob, error) {
+	var jobs []entity.TokenFragmentJob
+
+	limit := filter.PageSize
+	if limit == 0 {
+		limit = 5
+	}
+	if filter.Page == 0 {
+		filter.Page++
+	}
+
+	queryFilter := bson.M{}
+
+	if filter.Status > 0 {
+		queryFilter["status"] = filter.Status
+	}
+
+	options := options.Find()
+	options.SetSkip(int64((filter.Page - 1) * limit))
+	options.SetLimit(int64(limit))
+	options.SetSort(bson.M{"created_at": 1})
+
+	cursor, err := r.DB.Collection(utils.TOKEN_FILE_FRAGMENT_JOB).Find(ctx, queryFilter, options)
+	if err != nil {
+		return nil, err
+	}
+
+	if cursor.All(context.Background(), &jobs); err != nil {
+		return nil, err
+	}
+
+	return jobs, nil
+
 }
