93bcdf7e9dd23c0c7c73f455671d2e69c7818052
Hoang Phuong <phuong@autonomous.nyc>
2023-05-05 11:05:20 +0700

    rollback capcha

--- internal/delivery/http/faucet.go
+++ internal/delivery/http/faucet.go
@@ -5,8 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"go.uber.org/zap"
+	"gopkg.in/ezzarghili/recaptcha-go.v4"
 	"rederinghub.io/internal/delivery/http/request"
 	"rederinghub.io/internal/delivery/http/response"
 	faucetconst "rederinghub.io/utils/constants/faucet"
@@ -29,24 +31,24 @@ func (h *httpDelivery) requestFaucet(w http.ResponseWriter, r *http.Request) {
 		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
 		return
 	}
-	// if reqBody.RecaptchaResponse == "" {
+	if reqBody.RecaptchaResponse == "" {
 
-	// 	err = errors.New("the recaptcha is required.")
-	// 	logger.AtLog.Logger.Error("h.requestFaucet", zap.String("err", err.Error()))
-	// 	h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
-	// 	return
-	// }
+		err = errors.New("the recaptcha is required.")
+		logger.AtLog.Logger.Error("h.requestFaucet", zap.String("err", err.Error()))
+		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
+		return
+	}
 
-	// if len(h.Usecase.Config.CaptcharSecret) > 0 {
-	// 	captcha, _ := recaptcha.NewReCAPTCHA(h.Usecase.Config.CaptcharSecret, recaptcha.V3, 10*time.Second) // for v2 API get your secret from https://www.google.com/recaptcha/admin
+	if len(h.Usecase.Config.CaptcharSecret) > 0 {
+		captcha, _ := recaptcha.NewReCAPTCHA(h.Usecase.Config.CaptcharSecret, recaptcha.V3, 10*time.Second) // for v2 API get your secret from https://www.google.com/recaptcha/admin
 
-	// 	err = captcha.Verify(reqBody.RecaptchaResponse)
-	// 	if err != nil {
-	// 		logger.AtLog.Logger.Error("h.requestFaucet.recaptcha.Verify", zap.String("err", err.Error()))
-	// 		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
-	// 		return
-	// 	}
-	// }
+		err = captcha.Verify(reqBody.RecaptchaResponse)
+		if err != nil {
+			logger.AtLog.Logger.Error("h.requestFaucet.recaptcha.Verify", zap.String("err", err.Error()))
+			h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
+			return
+		}
+	}
 	result, err := h.Usecase.ApiCreateFaucet(reqBody.Address, reqBody.Url, reqBody.Txhash, reqBody.Type, reqBody.Source)
 	if err != nil {
 		logger.AtLog.Logger.Error("h.Usecase.GetFaucetPaymentInfo", zap.String("err", err.Error()))
