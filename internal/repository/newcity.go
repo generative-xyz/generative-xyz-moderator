70ab8841cddc03934a152aba1957b6662bcbbaf3
Genesis Doge <doge666@generative.xyz>
2023-05-11 15:23:25 +0700

    add api save new city faucet address

---
+++ internal/repository/newcity.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"rederinghub.io/internal/entity"
+	"rederinghub.io/utils/helpers"
+)
+
+func (r Repository) InsertNewCityGm(data *entity.NewCityGm) error {
+
+	data.Address = strings.ToLower(data.Address)
+	data.UserAddress = strings.ToLower(data.UserAddress)
+
+	err := r.InsertOne(data.TableName(), data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r Repository) FindNewCityGmByUserAddress(userAddress, typeReq string) (*entity.NewCityGm, error) {
+	resp := &entity.NewCityGm{}
+
+	filter := bson.D{{"user_address", strings.ToLower(userAddress)}, {"type", typeReq}}
+
+	usr, err := r.FilterOne(entity.NewCityGm{}.TableName(), filter)
+	if err != nil {
+		return nil, err
+	}
+
+	err = helpers.Transform(usr, resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
