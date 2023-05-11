8a4c984e9774447694e6fc1a50dc9b2c4c82b0cf
axe31803 <axe31803@generative.xyz>
2023-05-11 14:13:14 +0700

    [API] - Analytics collection's deposit - ETH

--- internal/delivery/http/chart.go
+++ internal/delivery/http/chart.go
@@ -28,9 +28,9 @@ func (h *httpDelivery) getChartDataFoTokenURI(w http.ResponseWriter, r *http.Req
 	dateRange := r.URL.Query().Get("dateRange")
 	f := utils.ParseAggregation(dateRange)
 	filter := &structure.AggerateChartForToken{
-		TokenID: &tokenID,
-		FromDate:  &f.FromDate,
-		ToDate:    &f.ToDate,
+		TokenID:  &tokenID,
+		FromDate: &f.FromDate,
+		ToDate:   &f.ToDate,
 	}
 
 	logger.AtLog.Logger.Info("getChartDataFoTokenURI.Filter", zap.Any("filter", zap.Any("filter)", filter)))
@@ -45,13 +45,11 @@ func (h *httpDelivery) getChartDataFoTokenURI(w http.ResponseWriter, r *http.Req
 }
 
 // UserCredits godoc
-// @Summary CollectionListing
-// @Description get list CollectionListing
+// @Summary GM deposit analytics
+// @Description GM deposit analytics
 // @Tags CollectionListing
 // @Accept  json
 // @Produce  json
-// @Param projectID path string  false "projectID"
-// @Param dateRange query string false "dateRange"
 // @Success 200 {object} response.JsonResponse{}
 // @Router /charts/collections/{projectID}/charts [GET]
 func (h *httpDelivery) getChartDataForCollection(w http.ResponseWriter, r *http.Request) {
@@ -65,7 +63,7 @@ func (h *httpDelivery) getChartDataForCollection(w http.ResponseWriter, r *http.
 		FromDate:  &f.FromDate,
 		ToDate:    &f.ToDate,
 	}
-	
+
 	logger.AtLog.Logger.Info("getChartDataForCollection.Filter", zap.Any("filter", zap.Any("filter)", filter)))
 	result, err := h.Usecase.GetChartDataOFProject(*filter)
 	if err != nil {
@@ -74,5 +72,24 @@ func (h *httpDelivery) getChartDataForCollection(w http.ResponseWriter, r *http.
 		return
 	}
 
+	h.Response.RespondSuccess(w, http.StatusOK, response.Success, result, "")
+}
+
+// UserCredits godoc
+// @Summary CollectionListing
+// @Description get list CollectionListing
+// @Tags Charts
+// @Accept  json
+// @Produce  json
+// @Param projectID path string  false "projectID"
+// @Param dateRange query string false "dateRange"
+// @Success 200 {object} response.JsonResponse{}
+// @Router /charts/gm-collections/deposit [GET]
+func (h *httpDelivery) getChartDataForGMCollection(w http.ResponseWriter, r *http.Request) {
+	result, err := h.Usecase.GetChartDataForGMCollection()
+	if err != nil {
+		h.Response.RespondWithError(w, http.StatusBadRequest, response.Error, err)
+		return
+	}
 	h.Response.RespondSuccess(w, http.StatusOK, response.Success, result, "")
 }
