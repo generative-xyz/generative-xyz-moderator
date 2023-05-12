2b5cd962b10a9762ae8d4349fc02773099405dc7
Genesis Doge <doge666@generative.xyz>
2023-05-12 12:09:03 +0700

    - add avatar

--- external/etherscan/structure.go
+++ external/etherscan/structure.go
@@ -14,6 +14,7 @@ type WalletAddressTxResponse struct {
 
 type AddressTxItemResponse struct {
 	ENS            string  `json:"ens"`
+	Avatar         string  `json:"avatar"`
 	From           string  `json:"from"`
 	To             string  `json:"to"`
 	Value          string  `json:"value"`
