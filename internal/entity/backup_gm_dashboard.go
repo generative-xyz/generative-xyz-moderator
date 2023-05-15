03216cad3e4e4bcb900d4165c5d52d099258854f
axe31803 <axe31803@generative.xyz>
2023-05-15 16:13:07 +0700

    gm-dashboard

---
+++ internal/entity/backup_gm_dashboard.go
@@ -0,0 +1,21 @@
+package entity
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"rederinghub.io/utils"
+	"rederinghub.io/utils/helpers"
+)
+
+type CachedGMDashBoard struct {
+	BaseEntity `bson:",inline"`
+	Value      *string `bson:"value"`
+	Key        string  `bson:"key"`
+}
+
+func (u CachedGMDashBoard) TableName() string {
+	return utils.COLLECTION_CACHED_GM_DASHBOARD
+}
+
+func (u CachedGMDashBoard) ToBson() (*bson.D, error) {
+	return helpers.ToDoc(u)
+}
