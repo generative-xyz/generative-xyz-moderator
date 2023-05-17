48767dc19bfa247d72b199f76ef33cf82fabf6fb
axe31803 <axe31803@generative.xyz>
2023-05-17 15:16:40 +0700

    [API] - Reallocated, save cache + db
    [API] - Reallocated, read backup data
    
    [API] - Reallocated done
    
    [API] - Reallocated done

---
+++ internal/entity/backup_gm_reallowcated_dashboard.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"rederinghub.io/utils"
+	"rederinghub.io/utils/helpers"
+)
+
+type CachedGMReAllocatedDashBoard struct {
+	BaseEntity   `bson:",inline"`
+	Contributors int     `bson:"contributors" json:"contributors"`
+	UsdtValue    float64 `bson:"usdtValue" json:"usdtValue"`
+
+	//only save uploaded link
+	BackupURL      string `bson:"backup_url" json:"backup_url"`
+	BackupFilePath string `bson:"backup_file_path" json:"backup_file_path"`
+	BackupFileName string `bson:"backup_file_name" json:"backup_file_name"`
+}
+
+func (u CachedGMReAllocatedDashBoard) TableName() string {
+	return utils.COLLECTION_CACHED_REALLOCATED_GM_DASHBOARD
+}
+
+func (u CachedGMReAllocatedDashBoard) ToBson() (*bson.D, error) {
+	return helpers.ToDoc(u)
+}
