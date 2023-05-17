162249845033854852d607dad9c23769b6265a76
Genesis Doge <doge666@generative.xyz>
2023-05-17 13:57:26 +0700

    - update

--- external/etherscan/structure.go
+++ external/etherscan/structure.go
@@ -13,15 +13,16 @@ type WalletAddressTxResponse struct {
 }
 
 type AddressTxItemResponse struct {
-	ENS            string  `json:"ens"`
-	Avatar         string  `json:"avatar"`
-	From           string  `json:"from"`
-	To             string  `json:"to"`
-	Value          string  `json:"value"`
-	UsdtValue      float64 `json:"usdt_value"`
-	UsdtValueExtra float64 `json:"usdt_value_extra"`
-	ExtraPercent   float64 `json:"extra_percent"`
-	Percent        float64 `json:"percent"`
-	GMReceive      float64 `json:"gm_receive"`
-	Currency       string  `json:"currency"`
+	ENS             string  `json:"ens"`
+	Avatar          string  `json:"avatar"`
+	From            string  `json:"from"`
+	To              string  `json:"to"`
+	Value           string  `json:"value"`
+	UsdtValue       float64 `json:"usdt_value"`
+	UsdtValueExtra  float64 `json:"usdt_value_extra"`
+	ExtraPercent    float64 `json:"extra_percent"`
+	Percent         float64 `json:"percent"`
+	GMReceive       float64 `json:"gm_receive"`
+	GMReceiveString string  `json:"gm_receive_string"`
+	Currency        string  `json:"currency"`
 }
